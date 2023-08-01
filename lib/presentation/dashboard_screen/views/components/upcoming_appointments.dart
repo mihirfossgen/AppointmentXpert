b40e4aee92fea7bebdf077ab3bbfa832345432a9
Mihir Rawal <mihirr@fossgentechnologies.com>
2023-08-01 18:16:13 +0530

    cancel appointment added

--- lib/presentation/dashboard_screen/views/components/upcoming_appointments.dart
+++ lib/presentation/dashboard_screen/views/components/upcoming_appointments.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:appointmentxpert/core/utils/time_calculation_utils.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:data_table_2/data_table_2.dart';
@@ -7,22 +9,26 @@ import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
 import '../../../../core/constants/app_constants.dart';
+import '../../../../core/utils/size_utils.dart';
 import '../../../../models/getAllApointments.dart';
 import '../../../../network/endpoints.dart';
 import '../../../../shared_prefrences_page/shared_prefrence_page.dart';
 import '../../../../theme/app_style.dart';
+import '../../../../widgets/custom_button.dart';
 import '../../../../widgets/custom_image_view.dart';
 import '../../../../widgets/responsive.dart';
 import '../../../patient_dashboard_screen/shared_components/card_appointment.dart';
 import '../../../patient_dashboard_screen/shared_components/responsive_builder.dart';
+import '../../controller/dashboard_controller.dart';
 
 class UpcomingAppointments extends StatelessWidget {
-  const UpcomingAppointments({
+  UpcomingAppointments({
     required this.data,
     Key? key,
   }) : super(key: key);
 
   final List<AppointmentContent> data;
+  DashboardController controller = Get.put(DashboardController());
 
   @override
   Widget build(BuildContext context) {
@@ -40,71 +46,144 @@ class UpcomingAppointments extends StatelessWidget {
         //width: 400,
         height: 250,
         child: Card(
-          child: AbsorbPointer(
-            child: DataTable2(
-                columnSpacing: 12,
-                horizontalMargin: 12,
-                minWidth: MediaQuery.of(Get.context!).size.width,
-                showBottomBorder: true,
-                columns: [
-                  DataColumn2(
-                    fixedWidth: 30,
-                    label: Text(
-                      'No',
-                      style: AppStyle.txtInterSemiBold14,
-                    ),
-                    //size: ColumnSize.L,
+          child: DataTable2(
+              columnSpacing: 12,
+              horizontalMargin: 12,
+              minWidth: MediaQuery.of(Get.context!).size.width,
+              showBottomBorder: true,
+              columns: [
+                DataColumn2(
+                  fixedWidth: 30,
+                  label: Text(
+                    'No',
+                    style: AppStyle.txtInterSemiBold14,
                   ),
-                  DataColumn(
-                    label: Text(
-                      'Name',
-                      style: AppStyle.txtInterSemiBold14,
-                    ),
+                  //size: ColumnSize.L,
+                ),
+                DataColumn2(
+                  label: Text(
+                    'Name',
+                    style: AppStyle.txtInterSemiBold14,
                   ),
-                  DataColumn(
-                    label: Text(
-                      'Time',
-                      style: AppStyle.txtInterSemiBold14,
-                    ),
+                ),
+                DataColumn2(
+                  label: Text(
+                    'Time',
+                    style: AppStyle.txtInterSemiBold14,
                   ),
-                  DataColumn(
-                    label: Text(
-                      'Status',
-                      style: AppStyle.txtInterSemiBold14,
-                    ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Status',
+                    style: AppStyle.txtInterSemiBold14,
                   ),
-                ],
-                rows: List<DataRow>.generate(
-                    data.length,
-                    (index) => DataRow(cells: [
-                          DataCell(
-                              Text(
-                                "${index + 1}",
-                                overflow: TextOverflow.clip,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              onTap: () {}),
-                          DataCell(
-                              Text(
-                                '${data[index].patient?.prefix}'
-                                '${data[index].patient?.firstName} '
-                                '${data[index].patient?.lastName}',
-                                overflow: TextOverflow.clip,
-                              ),
-                              onTap: () {}),
-                          DataCell(
-                              data[index].updateTimeInMin == 0
-                                  ? Text(
-                                      '${data[index].startTime?.replaceAll(' AM', '').replaceAll(' PM', '')}-${data[index].endTime}')
-                                  : Text(TimeCalculationUtils().timeCalCulated(
-                                      data[index].startTime,
-                                      data[index].endTime,
-                                      data[index].updateTimeInMin)),
-                              onTap: () {}),
-                          DataCell(Text('${data[index].status}')),
-                        ]))),
-          ),
+                ),
+                DataColumn2(
+                  label: Text(
+                    'Action',
+                    style: AppStyle.txtInterSemiBold14,
+                  ),
+                ),
+              ],
+              rows: List<DataRow>.generate(
+                  data.length,
+                  (index) => DataRow(cells: [
+                        DataCell(
+                            Text(
+                              "${index + 1}",
+                              overflow: TextOverflow.clip,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            onTap: () {}),
+                        DataCell(
+                            Text(
+                              '${data[index].patient?.prefix}'
+                              '${data[index].patient?.firstName} '
+                              '${data[index].patient?.lastName}',
+                              overflow: TextOverflow.ellipsis,
+                              softWrap: true,
+                            ),
+                            onTap: () {}),
+                        DataCell(
+                            data[index].updateTimeInMin == 0
+                                ? Text(
+                                    '${data[index].startTime?.replaceAll(' AM', '').replaceAll(' PM', '')}-${data[index].endTime}')
+                                : Text(TimeCalculationUtils().timeCalCulated(
+                                    data[index].startTime,
+                                    data[index].endTime,
+                                    data[index].updateTimeInMin)),
+                            onTap: () {}),
+                        DataCell(Text(
+                          '${data[index].status}',
+                          overflow: TextOverflow.ellipsis,
+                        )),
+                        DataCell(
+                          InkWell(
+                              onTap: () {
+                                WidgetsBinding.instance
+                                    .addPostFrameCallback((timeStamp) {
+                                  showDialog(
+                                    context: Get.context!,
+                                    builder: (context) => AlertDialog(
+                                      title: const Text(
+                                          'Do you want to cancel appointment?'),
+                                      actions: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            CustomButton(
+                                                height: getVerticalSize(60),
+                                                width: getHorizontalSize(80),
+                                                text: 'Yes',
+                                                margin: getMargin(
+                                                    left: 10, right: 00),
+                                                fontStyle: ButtonFontStyle
+                                                    .RalewayRomanSemiBold14WhiteA700,
+                                                onTap: () async {
+                                                  var req = {
+                                                    "active": false,
+                                                    "id": data[index].id,
+                                                    "date": data[index].date,
+                                                    "examinerId": data[index]
+                                                        .examiner!
+                                                        .id,
+                                                    "note": data[index].note,
+                                                    "patientId":
+                                                        data[index].patient?.id,
+                                                    "purpose":
+                                                        data[index].purpose,
+                                                    "status": "Canceled"
+                                                  };
+                                                  print(jsonEncode(req));
+                                                  controller
+                                                      .updateAppointment(req);
+                                                }),
+                                            CustomButton(
+                                                height: getVerticalSize(60),
+                                                width: getHorizontalSize(80),
+                                                text: 'No',
+                                                margin: getMargin(
+                                                    left: 0, right: 10),
+                                                fontStyle: ButtonFontStyle
+                                                    .RalewayRomanSemiBold14WhiteA700,
+                                                onTap: () async {
+                                                  Get.back();
+                                                })
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                });
+                              },
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              )),
+                        )
+                      ]))),
         ));
   }
 
